1d6b4d859cde8f3486b1eb5f58ee78322c40dacb
Mukilan <mukilankarthikeyan357@gmail.com>
2020-08-26 12:55:20 -0400

    2020.08.26 added field image as an background next step: is to change field image size to fit the screen

--- FTC_Vitrual_Robot_Simulation/src/WindowTest.java
+++ FTC_Vitrual_Robot_Simulation/src/WindowTest.java
@@ -1,3 +1,5 @@
+
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -6,7 +8,30 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
 public class WindowTest extends Application{
+
+    private Image field;
+    private ImageView fieldView;
+    Group mainGroup;
 
     Button tbut;
     public static void main(String[] args){
@@ -15,11 +40,19 @@ public class WindowTest extends Application{
     }
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Test Window");
-        tbut = new Button("testing");
-        StackPane layout = new StackPane();
-        layout.getChildren().add(tbut);
+        //tbut = new Button("testing");
+        //StackPane layout = new StackPane();
+        //layout.getChildren().add(tbut);
 
-        Scene scene = new Scene(layout, 500,500);
+        mainGroup = new Group();
+
+        field = new Image(new FileInputStream(System.getProperty("user.dir") + "/Skystone Field.png"));
+        fieldView = new ImageView();
+        fieldView.setImage(field);
+
+        mainGroup.getChildren().add(fieldView);
+
+        Scene scene = new Scene(mainGroup, 700,700);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
